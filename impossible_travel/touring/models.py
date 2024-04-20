be1fb2f153c62764c428fac892a320f0f826a674
AnnaElis <kunicasbumerangom@gmail.com>
2024-04-20 17:54:15 +0300

    созданы модели

--- impossible_travel/touring/models.py
+++ impossible_travel/touring/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+class Direction(models.Model):
+
+    name = models.CharField(max_length=50, help_text='Желаемая вселенная')
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('direction-detail', args=[str(self.id)])
+
+class Area(models.Model):
+
+    name = models.CharField(max_length=50, help_text='Желаемая местность')
+    description = models.CharField(max_length=500, help_text='Описание')
+    direction = models.ForeignKey('Direction', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('area-detail', args=[str(self.id)])
+
+class Commodity(models.Model):
+
+    name = models.CharField(max_length=30, help_text='Доступные удобства')
+
+    def __str__(self):
+        return self.name
+
+
+class Hotel(models.Model):
+
+    name = models.CharField(max_length=50, help_text='Название отеля')
+    description = models.CharField(max_length=500, help_text='Описание')
+    commodity = models.ManyToManyField('Commodity', help_text='Выберите доступные удобства')
+    cost_per_night = models.PositiveSmallIntegerField(help_text='Цена за ночь')   
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('hotel-detail', args=[str(self.id)])
+
+    
+class Entertaiment(models.Model):
+
+    name = models.CharField(max_length=50, help_text='Название отеля')
+    description = models.CharField(max_length=500, help_text='Описание')
+    area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True)
+    cost = models.PositiveSmallIntegerField(help_text='Цена за ночь', null=True)   
+
+    def __str__(self):
+        return self.name
+    
+class Tour(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Уникальный идентификатор тура")
+    hotel = models.ForeignKey('Hotel', on_delete=models.SET_NULL, null=True, blank=True)
+    checkin_date = models.DateField(help_text='Дата заселения')
+    checkout_date = models.DateField(help_text='Дата выезда')
+    entertaiments = models.ForeignKey('Entertaiment', help_text='Заказанные развлечения', on_delete=models.SET_NULL, null=True)
+    cost = models.PositiveSmallIntegerField(help_text='Итоговая стоимость')
+
+    def __str__(self):
+        return self.id
+
+
+
--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
 D:\Anna_python\impossible_travel\.imp_travel_env\Scripts
 D:\Anna_python\impossible_travel\.imp_travel_env\Scripts\activate.bat
 python -m pip install Django
+python manage.py runserver
+python manage.py makemigrations  
+python manage.py migrate  
