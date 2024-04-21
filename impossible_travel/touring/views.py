842a8bf9b3a2e6df01f8f8941e0ace098dfe01ba
AnnaElis <kunicasbumerangom@gmail.com>
2024-04-21 15:32:44 +0300

    отображается домашняя страница

--- impossible_travel/touring/views.py
+++ impossible_travel/touring/views.py
@@ -8,9 +8,11 @@ def index(request):
     num_directions=Direction.objects.all().count()
     num_areas=Area.objects.all().count()
     num_entertaiments=Entertaiment.objects.all().count()
+    num_hotels=Hotel.objects.all().count()
     context = {'num_directions':num_directions,
                'num_areas':num_areas,
-               'num_entertaiments':num_entertaiments
+               'num_entertaiments':num_entertaiments,
+               'num_hotels':num_hotels
                }
 
     return render(request, 'index.html', context=context)
