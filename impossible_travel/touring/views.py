176b0b8d0aacab1e2fdd725815af3d3680e6751e
AnnaElis <kunicasbumerangom@gmail.com>
2024-05-14 15:53:16 +0300

    изменение профиля и редирект

--- impossible_travel/touring/views.py
+++ impossible_travel/touring/views.py
@@ -125,6 +125,7 @@ def profile_edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            return HttpResponseRedirect(reverse('my_profile'))
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
